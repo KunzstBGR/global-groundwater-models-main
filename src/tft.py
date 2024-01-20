012accf156b97469d1a21971249eba95ed53a1c2
Teo <teo_chiaburu@yahoo.com>
2024-01-20 16:42:35 +0100

    Started one epoch training TFT

--- src/tft.py
+++ src/tft.py
@@ -1,17 +1,14 @@
+import sys
+sys.path.insert(1, '/mnt/KInsektDaten/teo/pytorch-forecasting')
 import os
 import torch
-import numpy as np
-import pandas as pd
-import torch.nn as nn
 
-# import pytorch_lightning as pl
 import lightning.pytorch as pl
 from lightning.pytorch import seed_everything
 from lightning.pytorch.strategies import DDPStrategy
-# from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.loggers import TensorBoardLogger
-from lightning.pytorch.callbacks import EarlyStopping
-from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks import EarlyStopping, StochasticWeightAveraging, LearningRateMonitor
+from lightning.pytorch.callbacks import ModelCheckpoint, TQDMProgressBar
 import warnings
 warnings.filterwarnings('ignore')
 import matplotlib
@@ -19,19 +16,18 @@ matplotlib.use("Agg") # to prevent the allocate bitmap warning
 
 from pytorch_forecasting.models.temporal_fusion_transformer import TemporalFusionTransformer
 from pytorch_forecasting import TimeSeriesDataSet
-from pytorch_forecasting.metrics import MultiHorizonMetric
 from pytorch_forecasting.metrics import MAE, MAPE, SMAPE, RMSE, QuantileLoss
 
 # Setup
-BASE_PATH = 'D:/KIMoDIs/global-groundwater-models-main'
-DATA_PATH = os.path.join(BASE_PATH, 'data')
-MODEL_PATH = os.path.join(BASE_PATH, 'models')
-RESULT_PATH = os.path.join(BASE_PATH, 'results')
-SHARE_PATH = 'J:/Berlin/B22-FISHy/NUTZER/Kunz.S/kimodis_preprocessed'
+#BASE_PATH = 'D:/KIMoDIs/global-groundwater-models-main'
+DATA_PATH = '../../../data/Grundwasser/kimodis_preprocessed/data/'
+#MODEL_PATH = os.path.join(BASE_PATH, 'models')
+#RESULT_PATH = os.path.join(BASE_PATH, 'results')
+SHARE_PATH = '../../../data/Grundwasser/kimodis_preprocessed/'
 
 # Specify model type
-MODEL_TYPE = 'dyn' # full # full_interpol
-BATCH_SIZE = 4096
+#MODEL_TYPE = 'dyn' # full # full_interpol
+BATCH_SIZE = 32 #4096
 VERSION = '10_Epochs'
 
 
@@ -42,19 +38,19 @@ VERSION = '10_Epochs'
 # [self.hparams.x_reals.index(name) for name in to_list(target)],
 # ValueError: 'gwl' is not in list
 # - dyn: only dynamic input features
-MODEL_TYPE_ls = ['full', 'full_interpol' ,'dyn'] 
-seeds = [94, 673, 1899, 2149, 9898]
+MODEL_TYPE_ls = ['full']#, 'full_interpol','dyn']
+seeds = [94]#, 673, 1899, 2149, 9898]
 
 for MODEL_TYPE in MODEL_TYPE_ls: 
     
     # Load TFT TimeSeriesDataSets and create dataloaders
     train_ds = TimeSeriesDataSet.load(os.path.join(SHARE_PATH, f'train_ds_{MODEL_TYPE}_tft.pt'))
     print(len(train_ds.decoded_index['proj_id'].unique()), 'sites are in the training data.')
-    train_dataloader = train_ds.to_dataloader(train=True, batch_size=BATCH_SIZE, num_workers=0)
+    train_dataloader = train_ds.to_dataloader(train=True, batch_size=BATCH_SIZE, num_workers=18)
 
     val_ds = TimeSeriesDataSet.load(os.path.join(SHARE_PATH, f'val_ds_{MODEL_TYPE}_tft.pt'))
     print(len(val_ds.decoded_index['proj_id'].unique()), 'sites are in the validation data.')
-    val_dataloader = val_ds.to_dataloader(train=False, batch_size=BATCH_SIZE, num_workers=0)
+    val_dataloader = val_ds.to_dataloader(train=False, batch_size=BATCH_SIZE, num_workers=18)
     
     for i in seeds:
         seed_everything(i, workers = True)
@@ -73,19 +69,18 @@ for MODEL_TYPE in MODEL_TYPE_ls:
         # Logging metrics: SMAPE, MAE, RMSE, MAPE
         # Metrics: 
         # https://pytorch-forecasting.readthedocs.io/en/stable/api/pytorch_forecasting.metrics.point.RMSE.html#
-        model = TemporalFusionTransformer.from_dataset(train_ds, 
-                                                      loss=QuantileLoss()) # QuantileLoss()
+        model = TemporalFusionTransformer.from_dataset(train_ds, loss=QuantileLoss())
 
         # Logger
-        tb_logger = TensorBoardLogger(save_dir=f'D:/KIMoDIs/global-groundwater-models-main/models/tft/tft_{MODEL_TYPE}', 
-                                     name=f'tft_{MODEL_TYPE}_{BATCH_SIZE}_{VERSION}')
+        tb_logger = TensorBoardLogger(save_dir=f'./models/tft/tft_{MODEL_TYPE}_teo',
+                                      name=f'tft_{MODEL_TYPE}_{BATCH_SIZE}_{VERSION}_teo')
 
         # Early stopping
         # Patience is the number of checks with no improvement after which training will be stopped
         # Default: one check after every training epoch 
         # Also connected with check_val_every_n_epoch (set in trainer)
         # https://lightning.ai/docs/pytorch/2.0.4/api/lightning.pytorch.callbacks.EarlyStopping.html
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=5, verbose=False, mode="min")
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=10, verbose=True, mode="min")
 
         # Checkpoint callback
         # Used to save the best model (or top-k best models)
@@ -94,19 +89,23 @@ for MODEL_TYPE in MODEL_TYPE_ls:
         # trainer.test(best_model)
         checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min")
 
+        # Schedule to reduce the learning rate throughout training
+        lr_sched = StochasticWeightAveraging(swa_lrs=1e-3, swa_epoch_start=5, device=torch.device('cuda:0'))
+        lr_logger = LearningRateMonitor(logging_interval='step')  # log the learning rate ('step' or 'epoch')
+
         # Training
         # https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.trainer.trainer.Trainer.html
         trainer = pl.Trainer(
             max_epochs=10,
             accelerator='gpu', 
-            devices=4,
+            devices=1,
             enable_model_summary=True,
             strategy=ddp,
-            fast_dev_run=True,
-            callbacks=[early_stop_callback, checkpoint_callback],
+            #fast_dev_run=True, runs only one batch to check whether there are bugs
+            callbacks=[early_stop_callback, checkpoint_callback, lr_sched, lr_logger, TQDMProgressBar()],
             logger=tb_logger,
-            log_every_n_steps=10,  # Default = 50 (steps)
-            val_check_interval=0.2 # Check after 50% of each epoch
+            log_every_n_steps=5,  # Default = 50 (steps)
+            #val_check_interval=0.2 # Check after 50% of each epoch
         )
         trainer.fit(
             model,
